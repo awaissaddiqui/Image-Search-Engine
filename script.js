d574a9b14f6e009837bf5fc472d81b88e4fc5671
AWAIS SADDIQUI <awaissaddiqui143@gmail.com>
2022-08-18 22:10:43 -0700

    Change Image selection criteria from API

--- script.js
+++ script.js
@@ -10,11 +10,12 @@ const loadPics = async () => {
         fetch(`https://api.unsplash.com/search/photos?client_id=${key}&query=${term}`)
         .then((res) => res.json())
         .then((json) => {
-            const pics = json.results.map(res => {
+            const pics = json.results;
+                    pics.map(res => {
                 //console.log(pics)
 
                     const img = document.createElement("img")
-                    img.setAttribute("src",res.urls.raw)
+                    img.setAttribute("src",res.urls.small)
                     img.setAttribute("width", 300)
                     img.setAttribute("height", 300)
 
